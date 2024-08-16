9ca6b57658e6cac78ebab4cedaefe4e65423150d
ChndrshP <chndrsh25@gmail.com>
2024-08-16 19:39:14 +0530

    cleanup 16/08/24

--- server/database/conn.js
+++ server/database/conn.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 //Database Connection
-mongoose.connect('connection-string');
+mongoose.connect('mongodb+srv://chandresh2003cp:PhlEToZtF8woxoLN@cluster0.yiomq.mongodb.net/');
 
 //Exporting the connection
 export default mongoose;
